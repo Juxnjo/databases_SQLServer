3e82549f34afc8eeb46db653e0c5e1e2240a9209
“Juan <ramirezjuanj0117@gmail.com>
2023-08-03 18:50:54 -0500

    datos insertados en las tablas

--- empleadosDB.sql
+++ empleadosDB.sql
@@ -40,6 +40,14 @@ INSERT INTO cargos (id_car, nom, salario)
 VALUES (7, 'Operario', 990), (9, 'Ayudante', 780), (11, 'Secretaria Gral', 1100),
 		(20, 'Jefe Contabilidad', 3900), (50, 'Gerente General', 7200);
 
+INSERT INTO empleados (id_emp, nomb, apell, nacimiento, id_car, id_ciu, direccion, cel)
+VALUES (702040, 'Juan', 'Molina', '01/01/1989', 9, 'Tulu', 'Calle 15 10 10', 3503030),
+		(704040, 'Andres', 'Perez', '03/25/1987', 20, 'Gira', 'Circ 2 120 25', 2802030),
+		(706020, 'Marina', 'Carrillo', '02/28/1980', 7, 'Cali', 'Kra 5 sur 1d 45', 3416090),
+		(708030, 'Rogelio', 'Cabrales', '05/15/1978', 50, 'Tulu', 'Autop sur Kmt 25', 0),
+		(705030, 'Diana', 'Moncada', '01/15/1985', 20, 'Mede', 'Diag 25 14 20', 2501030),
+		(703050, 'Carlos', 'Carrillo', '12/13/1988', 11, 'Mede', 'Circunv 15 102 32', 0)
+
 
 --Mostrar todos los datos
 
