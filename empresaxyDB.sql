437a501d4987a03082cba53504b3f617c319a4af
“Juan <ramirezjuanj0117@gmail.com>
2023-08-08 09:27:35 -0500

    empresaxydb insert

--- empresaxyDB.sql
+++ empresaxyDB.sql
@@ -29,3 +29,8 @@ INSERT INTO productos (nombre, valor)
 VALUES ('Grafica', 599), ('Monitor', 999), ('Mouse', 35), ('Teclado', 100), ('Diadema', 99),
 		('Procesador', 499), ('Board', 249), ('RAM', 99), ('SSD', 100), ('HDD', 59)
 
+INSERT INTO clientes(nombre, apellido, cel, ingresos)
+VALUES ('Juan', 'Petro', 1854545 , 200 ), ('Sandra', 'Cano', 5454754, 0), ('Randy', 'Orton', 54564564 , 0 ),
+		('Leandro', 'Sins', 324545 , 1 ), ('Lucas', 'Cano', 545423, 99), ('Paco', 'Elche', 54564 , 2 ),
+		('Jose', 'Madrid', 1854535 , 200 ), ('Sandra', 'Perez', 5454754, 0), ('Walter', 'Mailo', 54564 , 99 )
+
